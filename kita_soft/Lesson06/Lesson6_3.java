085a45936ada5719aed9b9324e2c3f51c9361833
instantheven <neru.lovesong@gmail.com>
2020-01-31 15:20:58 +0900

    Lesson6_3.java追加

--- kita_soft/Lesson06/Lesson6_3.java
+++ kita_soft/Lesson06/Lesson6_3.java
@@ -2,16 +2,21 @@ import java.util.*;
 public class Lesson6_3{
 	public static void main(String[] args){
 		System.out.print("ひとつめの整数>");
-		int num1=new Scanner(System.in).nextInt();
+		int x=new Scanner(System.in).nextInt();
 		System.out.print("ふたつめの整数>");
-		int num2=new Scanner(System.in).nextInt();
-		System.out.printf("%dと%dでは%dの方が最も大きい数値です",num1,num2,maxOf(num1,num2));
+		int y=new Scanner(System.in).nextInt();
+		System.out.print("みっつめの整数>");
+		int z=new Scanner(System.in).nextInt();
+		int a=maxOf(x,y);
+		System.out.printf("%dと%dと%dでは%dの方が最も大きい数値です",x,y,z,maxOf(a,z));
 	}
 	public static int maxOf(int x,int y){
+		int ans=0;
 		if(x>y){
-			return x;
+			ans=x;
 		}else if(x<y){
-			return y;
+			ans=y;
 		}
+		return ans;
 	}
 }
