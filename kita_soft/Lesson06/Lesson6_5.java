eb08956409aabe48d4bab7412c2a1f260c2c033a
instantheven <neru.lovesong@gmail.com>
2020-02-04 12:39:44 +0900

    rpg追記、Lesson6_5.java追記

--- kita_soft/Lesson06/Lesson6_5.java
+++ kita_soft/Lesson06/Lesson6_5.java
@@ -3,12 +3,13 @@ public class Lesson6_5{
 	public static void main(String[] args){
 		Scanner scan=new Scanner(System.in);
 		System.out.print("サイズ>");
-		int sizeNum=scan.nextInt();scan.nextLine();//次の入力がスキップしないように
+		//次の入力がスキップしないようにnextLine()
+		int sizeNum=scan.nextInt();scan.nextLine();
 		System.out.print("記号>");
 		String mark=scan.nextLine();
 		triangle(sizeNum,mark);//サイズと記号を渡す/図形を受け取る
 	}
-	public static void triangle(int a,String b){
+	public static void triangle(int a,String b){//サイズと記号を受け取る
 		for(int i=0;i<a;i++){//サイズ分回す
 			for(int j=0;j<=i;j++){
 				System.out.print(b);//記号を出力
